efc645938f4c8fca0d172ba60c7aa6923d33cdec
cihancalli <cihan.callii@gmail.com>
2022-05-20 20:47:37 +0300

    fetch Data From API

--- app/src/main/java/com/zerdasoftware/nutrientsbook/viewmodel/NutrientListViewModel.kt
+++ app/src/main/java/com/zerdasoftware/nutrientsbook/viewmodel/NutrientListViewModel.kt
@@ -3,21 +3,48 @@ package com.zerdasoftware.nutrientsbook.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.zerdasoftware.nutrientsbook.model.Nutrient
+import com.zerdasoftware.nutrientsbook.service.NutrientAPIService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
 
 class NutrientListViewModel : ViewModel() {
     val Nutrients = MutableLiveData<List<Nutrient>>()
     val NutrientErrorMessage = MutableLiveData<Boolean>()
     val NutrientLoading = MutableLiveData<Boolean>()
 
+    private val NutrientAPIService = NutrientAPIService()
+    //Kullan at
+    private val disposable = CompositeDisposable()
+
 
     fun refreshData(){
-        val banana = Nutrient("banana", "100","10","5","1","https://www.onlygfx.com/wp-content/uploads/2020/02/banana-1.png")
-        val strawberry = Nutrient("strawberry", "200","20","6","2","https://i.pinimg.com/originals/28/3e/53/283e53880ea4fd483c4968d89b143866.png")
-        val apple = Nutrient("apple", "300","30","7","3","https://pngimg.com/uploads/apple/apple_PNG12405.png")
+        fetchDataFromAPI()
+    }
 
-        val NutrientList = arrayListOf<Nutrient>(banana,strawberry,apple)
-        Nutrients.value = NutrientList
-        NutrientErrorMessage.value = false
-        NutrientLoading.value = false
+    private fun fetchDataFromAPI(){
+        NutrientLoading.value = true
+        disposable.add(
+            NutrientAPIService.getData()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object : DisposableSingleObserver<List<Nutrient>>() {
+                    override fun onSuccess(t: List<Nutrient>) {
+                        //Başarılı Olursa
+                        Nutrients.value = t
+                        NutrientErrorMessage.value = false
+                        NutrientLoading.value = false
+                    }
+
+                    override fun onError(e: Throwable) {
+                        //Hata alırsak
+                        NutrientErrorMessage.value = true
+                        NutrientLoading.value = false
+                        e.printStackTrace()
+                    }
+
+                })
+        )
     }
 }
