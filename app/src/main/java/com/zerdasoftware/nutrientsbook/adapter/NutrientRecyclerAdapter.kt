c823fb3deb7b35a21858b1c4633dd648edb2e753
cihancalli <cihan.callii@gmail.com>
2022-05-22 16:00:32 +0300

    Data Binding Adapter

--- app/src/main/java/com/zerdasoftware/nutrientsbook/adapter/NutrientRecyclerAdapter.kt
+++ app/src/main/java/com/zerdasoftware/nutrientsbook/adapter/NutrientRecyclerAdapter.kt
@@ -3,23 +3,23 @@ package com.zerdasoftware.nutrientsbook.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.zerdasoftware.nutrientsbook.R
+import com.zerdasoftware.nutrientsbook.databinding.NutrientRecyclerRowBinding
 import com.zerdasoftware.nutrientsbook.model.Nutrient
-import com.zerdasoftware.nutrientsbook.util.CreatePlaceholder
-import com.zerdasoftware.nutrientsbook.util.fetchImage
-import com.zerdasoftware.nutrientsbook.view.NutrientsListFragmentDirections
-import kotlinx.android.synthetic.main.nutrient_recycler_row.view.*
 
 class NutrientRecyclerAdapter(val NutrientList : ArrayList<Nutrient>) :RecyclerView.Adapter<NutrientRecyclerAdapter.NutrientViewHolder>() {
-    class NutrientViewHolder(itemView:View) :RecyclerView.ViewHolder(itemView){
+
+    class NutrientViewHolder(var view:NutrientRecyclerRowBinding) :RecyclerView.ViewHolder(view.root){
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NutrientViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val view = inflater.inflate(R.layout.nutrient_recycler_row,parent,false)
+        //val view = inflater.inflate(R.layout.nutrient_recycler_row,parent,false)
+        val view = DataBindingUtil.inflate<NutrientRecyclerRowBinding>(inflater,R.layout.nutrient_recycler_row,parent,false )
         return NutrientViewHolder(view)
     }
 
@@ -28,18 +28,21 @@ class NutrientRecyclerAdapter(val NutrientList : ArrayList<Nutrient>) :RecyclerV
     }
 
     override fun onBindViewHolder(holder: NutrientViewHolder, position: Int) {
+        holder.view.nutrientRow = NutrientList[position]
+
+
+        /*
         holder.itemView.textViewNutrientTitleRow.text = NutrientList.get(position).nutrientTitle
         holder.itemView.textViewNutrientCalorieRow.text = NutrientList.get(position).nutrientCalorie
-
         holder.itemView.setOnClickListener {
             val action = NutrientsListFragmentDirections.actionNutrientsListFragmentToNutrientsDetailFragment(NutrientList.get(position).uuid)
             Navigation.findNavController(it).navigate(action )
         }
-
         NutrientList.get(position).nutrientImage?.let {
             holder.itemView.imageViewNutrientRow.fetchImage(
                 it,CreatePlaceholder(holder.itemView.context))
         }
+         */
     }
 
     fun NutrientListUpdate(newNutrientList: List<Nutrient>){
